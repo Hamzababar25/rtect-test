42d5e1d6af9b561fa6368147f55adff6e44e77cc
Hamzababar25 <hamzabava70@gmail.com>
2025-01-31 17:50:45 +0500

    date picker adjusted

--- app/Dashboard/page.js
+++ app/Dashboard/page.js
@@ -1,13 +1,18 @@
+"use client";
 import { Calendar } from "lucide-react";
 
-import React from "react";
+import React, { useState, useRef } from "react";
 import Header from "../../components/Header";
 import Image from "next/image";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 const Dashboard = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const startDateRef = useRef(null);
+  const endDateRef = useRef(null);
   return (
     <main className="flex-1  px-5 pt-2">
-      {/* <Header /> */}
-
       <section className="grid grid-cols-3 lg:grid-cols-[1fr_1fr_1.5fr] xl:grid-cols-3 gap-2 mt-6 2xl:w-11/12 mx-auto">
         {/* Stat Cards */}
         <div className="bg-[#14081E]  flex items-center h-32 p-4 pl-8 rounded-2xl shadow-md">
@@ -64,19 +69,38 @@ const Dashboard = () => {
           <div className="mt-3">
             <label className="text-sm text-gray-400">Start Date</label>
             <div className="flex items-center bg-[#3A2D56] p-3 rounded-lg mt-1">
-              <Calendar className="text-gray-400" size={18} />
-              <span className="ml-3">07-05-2025</span>
+              <Calendar
+                className="text-gray-400 cursor-pointer"
+                size={18}
+                onClick={() => startDateRef.current.setFocus()}
+              />
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => setStartDate(date)}
+                dateFormat="dd-MM-yyyy"
+                className="ml-3 bg-transparent text-white outline-none w-full"
+                ref={startDateRef}
+              />
             </div>
           </div>
 
           <div className="mt-3">
             <label className="text-sm text-gray-400">End Date</label>
             <div className="flex items-center bg-[#3A2D56] p-3 rounded-lg mt-1">
-              <Calendar className="text-gray-400" size={18} />
-              <span className="ml-3">12-05-2025</span>
+              <Calendar
+                className="text-gray-400 cursor-pointer"
+                size={18}
+                onClick={() => endDateRef.current.setFocus()}
+              />
+              <DatePicker
+                selected={endDate}
+                onChange={(date) => setEndDate(date)}
+                dateFormat="dd-MM-yyyy"
+                className="ml-3 bg-transparent text-white outline-none w-full"
+                ref={endDateRef}
+              />
             </div>
           </div>
-
           <button className="w-full mt-10 h-14 bg-gradient-to-r from-blue-400 to-purple-500 py-2 rounded-lg font-bold">
             Download
           </button>
